bbc4f95f523ceb0a78fa129620bcf2fd9e72354c
DESKTOP-K0GMEED\Jhon <jaguiar@asesoftware.com>
2024-02-12 10:35:11 -0500

    Ajuste dias #9508

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
+        s
       </div>
     </Router>
   );
